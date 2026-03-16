d3fbafa3ca7481b89d6407b3bd34e4301a6cbaef
Gustavo Pigaiani <gupigaiani@gmail.com>
2026-03-16 16:21:42 -0300

    Atualizando dados com PATCH

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
