5b66eb0002439dd17a1e9affc77cce462cf51bf3
Gustavo Pigaiani <gupigaiani@gmail.com>
2026-03-20 21:01:50 -0300

    Alterando tipo de deleção no OnModelCreating

--- Migrations/20260320235929_DeleteRestrict.Designer.cs
+++ Migrations/20260320235929_DeleteRestrict.Designer.cs
@@ -3,6 +3,7 @@ using FilmesApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FilmesApi.Migrations
 {
     [DbContext(typeof(FilmeContext))]
-    partial class FilmeContextModelSnapshot : ModelSnapshot
+    [Migration("20260320235929_DeleteRestrict")]
+    partial class DeleteRestrict
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
